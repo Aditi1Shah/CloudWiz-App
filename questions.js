8891b78258a1988ecc15a31842cc5730719a4f30
Aditi Shah <72251182+Aditi1Shah@users.noreply.github.com>
2024-07-12 08:18:30 -0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions= [
+    {
+        numb:1,
+        question:"What does HTML stand for?",
+        answer :"B. Hyper Text Markup Language",
+        options:[
+            "A. Hyper Type Markup Language",
+            "B. Hyper Text Markup Language",
+            "C. Huge Type Material Literal",
+            "D. Hyer Title Market Language"
+        ]
+    },
+    {
+        numb:2,
+        question:"Which of the following HTML Elements is used for making any text bold ?",
+        answer :"D. Bold Tag",
+        options:[
+            "A. List Tag",
+            "B. Tab Tag",
+            "C. Unordere List Tag",
+            "D. Bold Tag"
+        ]
+    },
+    {
+        numb:3,
+        question:"Which of the following command is used to create react-js-app ?",
+        answer :"A. npx create-react-app appname",
+        options:[
+            "A. npx create-react-app appname",
+            "B. npm install create-react-app",
+            "C. npx install create-react-app -g",
+            "D. install - l create-react-app"    
+        ]
+    },
+    {
+        numb:4,
+        question:"Which of the following is a way to handle data in React.js ?",
+        answer :"A. State & Props",
+        options:[
+            "A. State & Props",
+            "B. Services & Components",
+            "C. State & Services",
+            "D. State & Component"
+        ]
+    },
+    {
+        numb:5,
+        question:"What does HTML stand for?",
+        answer :"B. Hyper Text Markup Language",
+        options:[
+            "A. Hyper Type Markup Language",
+            "B. Hyper Text Markup Language",
+            "C. Huge Type Material Literal",
+            "D. Hyer Title Market Language"
+        ]
+    }
+]
